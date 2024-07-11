628925a2d00955c94795db7fe5c5b0fb18f57a8b
Ginger Heef <gingerheef003@gmail.com>
2024-07-11 20:29:46 +0530

    lc1

---
+++ leetcode/two-sum.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/two-sum/
+
+#include vector
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            int comp = target - nums[i];
+            if(mp.find(comp) != mp.end()) return {mp[comp], i};
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
